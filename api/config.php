4bd956afdc233015c5f6ee02c580b6d1530c0f93
Priya Kuril <educatengneer235@gmail.com>
2026-05-08 00:32:09 +0530

    My Portfolio

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+// api/config.php
+// Update these values if needed.
+
+return [
+    'db_host' => '127.0.0.1',
+    'db_name' => 'portfolio_db',
+    'db_user' => 'root',
+    'db_pass' => 'Piyu@26#',
+];
+
+
+?>
